cc07d0fe61021a35b152e3488a0a14602e966dee
harish-b <harish.basavant@gmail.com>
2023-04-17 12:39:09 +0100

    Update index.js

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Jism part1' });
+  res.render('index', { title: 'Jonny part1' });
 });
 
 module.exports = router;
